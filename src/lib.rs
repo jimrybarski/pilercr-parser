d7cf0001abf103cd784f67f92909164b2cceb745
Jim Rybarski <jim@rybarski.com>
2023-02-06 17:01:03 -0600

    add parser for a full array and a test. this is broken because pilercr incorrectly reports coordinates if the repeat sequences have gaps in them, so some refactoring will be required to handle that

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::borrow::Cow;
-
 use nom::{
     bytes::complete::tag,
     character::complete::{
@@ -25,6 +23,16 @@ struct RepeatSpacer<'a> {
     end: usize,
     repeat: String,
     spacer: &'a str,
+}
+
+#[derive(Debug, PartialEq)]
+struct Array<'a> {
+    accession: &'a str,
+    order: usize,
+    start: usize,
+    end: usize,
+    consensus_repeat_sequence: &'a str,
+    repeat_spacers: Vec<RepeatSpacer<'a>>,
 }
 
 fn not_space(input: &str) -> IResult<&str, &str> {
@@ -103,24 +111,49 @@ fn parse_array_summary_line(input: &str) -> IResult<&str, &str> {
     }
 }
 
-// fn parse_array(input: &str) -> IResult<&str, Array<'a>> {
-//     let result = tuple((
-//         tag("Array "),
-//         digit1,
-//         line_ending,
-//         char('>'),
-//         not_space,
-//         line_ending,
-//         skip_empty_line,
-//         skip_one_line,
-//         skip_one_line,
-//         many1(parse_raw_repeat_spacer),
-//         skip_one_line,
-//         parse_array_summary_line,
-//         skip_empty_line,
-//         skip_empty_line,
-//     ))(input);
-// }
+fn parse_array(input: &str) -> IResult<&str, Array> {
+    let result = tuple((
+        tag("Array "),
+        digit1,
+        line_ending,
+        char('>'),
+        not_space,
+        line_ending,
+        skip_empty_line,
+        skip_one_line,
+        skip_one_line,
+        many1(parse_raw_repeat_spacer),
+        skip_one_line,
+        skip_one_line,
+        parse_array_summary_line,
+        skip_empty_line,
+        skip_empty_line,
+    ))(input);
+    match result {
+        Err(e) => Err(e),
+        Ok((remainder, data)) => {
+            let order = data.1.parse::<usize>().unwrap() - 1;
+            let accession = data.4;
+            let raw_repeat_spacers = data.9;
+            let consensus_repeat_sequence = data.12;
+            let repeat_spacers =
+                convert_raw_rs_to_final_rs(consensus_repeat_sequence, &raw_repeat_spacers);
+            let start = repeat_spacers.first().unwrap().start;
+            let end = repeat_spacers.last().unwrap().end;
+            Ok((
+                remainder,
+                Array {
+                    start,
+                    end,
+                    order,
+                    accession,
+                    consensus_repeat_sequence,
+                    repeat_spacers,
+                },
+            ))
+        }
+    }
+}
 
 fn convert_raw_rs_to_final_rs<'a>(
     consensus_repeat: &'a str,
@@ -195,18 +228,49 @@ mod tests {
         assert_eq!(rs[1].repeat, "GGGTTTCCGTCCCCTTCGGGGAATCATTTAGAAAATA");
     }
 
-    //     #[test]
-    //     fn parse_array() {
-    //         let input = "Array 18
-    // >MGYG000232241_150
+    #[test]
+    fn test_parse_array() {
+        let input = "Array 18
+>MGYG000232241_150
 
-    //        Pos  Repeat     %id  Spacer  Left flank    Repeat                                      Spacer
-    // ==========  ======  ======  ======  ==========    ========================================    ======
-    //       3832      40    92.5      34  CATATAGCAA    ..A..................................CC.    GAATTACATCGTATGCCAATACGCAGTTGCTTTT
-    //       3906      40    97.5      41  AGTTGCTTTT    .....................................---    TGTACTACTATGCGGTATTCCATCTGAAGGATGGCGGCTAC
-    //       3987      40    92.5          TGGCGGCTAC    GG............-......................--.    ATCACATTCA
-    // ==========  ======  ======  ======  ==========    ========================================
-    //          3      40              37                AAGTTTCCGTCCCCTTTCGGGGAATCATTTAGAAAAT--A
+       Pos  Repeat     %id  Spacer  Left flank    Repeat                                      Spacer
+==========  ======  ======  ======  ==========    ========================================    ======
+      3832      40    92.5      34  CATATAGCAA    ..A..................................CC.    GAATTACATCGTATGCCAATACGCAGTTGCTTTT
+      3906      40    97.5      41  AGTTGCTTTT    .....................................---    TGTACTACTATGCGGTATTCCATCTGAAGGATGGCGGCTAC
+      3987      40    92.5          TGGCGGCTAC    GG............-......................--.    ATCACATTCA
+==========  ======  ======  ======  ==========    ========================================
+         3      40              37                AAGTTTCCGTCCCCTTTCGGGGAATCATTTAGAAAAT--A
 
-    // ";
+
+";
+        let expected = Array {
+            accession: "MGYG000232241_150",
+            consensus_repeat_sequence: "AAGTTTCCGTCCCCTTTCGGGGAATCATTTAGAAAAT--A",
+            start: 3831,
+            end: 4034,
+            order: 17,
+            repeat_spacers: vec![
+                RepeatSpacer {
+                    start: 3831,
+                    end: 3905,
+                    spacer: "GAATTACATCGTATGCCAATACGCAGTTGCTTTT",
+                    repeat: "AAATTTCCGTCCCCTTTCGGGGAATCATTTAGAAAATCCA".to_string(),
+                },
+                RepeatSpacer {
+                    start: 3905,
+                    end: 3983,
+                    spacer: "TGTACTACTATGCGGTATTCCATCTGAAGGATGGCGGCTAC",
+                    repeat: "AAGTTTCCGTCCCCTTTCGGGGAATCATTTAGAAAAT".to_string(),
+                },
+                RepeatSpacer {
+                    start: 3983,
+                    end: 4030,
+                    spacer: "ATCACATTCA",
+                    repeat: "GGGTTTCCGTCCCCTTTCGGGGAATCATTTAGAAAATA".to_string(),
+                },
+            ],
+        };
+        let (_, actual) = parse_array(input).unwrap();
+        assert_eq!(expected, actual);
+    }
 }
